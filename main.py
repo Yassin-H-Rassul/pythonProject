ef5e7cff2b548c1c44d47837232ec1c056d5ee0b
Yassin Rassul <yassinhrassul@gmail.com>
2022-07-04 15:54:48 +0300

    2nd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
 import nnfs
-
+# hello it is me
 # x_train is the input variable (size in 1000 square feet)
 # y_train is the target (price in 1000s of dollars)
 nnfs.init()
